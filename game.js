1acfb17f115b676a1a7edcee7dfccde2470d27b8
EddieSherban <eddiesrbn3@gmail.com>
2026-02-17 20:07:37 -0800

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "lime";
+ctx.fillRect(72, 72, 16, 16); // pixel player
+
 let x = 72, y = 72;
 
 document.addEventListener("keydown", e => {
